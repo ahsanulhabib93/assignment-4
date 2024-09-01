dfd2cd089092739341dcb126026c0f1c4dc1d1d7
Md. Ahsanul Habib <ahsanulhabib1993@gmail.com>
2024-09-02 01:58:39 +0600

    test

--- FLASK_STARTERKIT/auth.py
+++ FLASK_STARTERKIT/auth.py
@@ -6,6 +6,6 @@ auth_routes = Blueprint("auth_routes", __name__)
 @auth_routes.route('/')
 def welcome():
     return {
-        "message":  "Welcome to your awesome auth endpoint yes",
+        "message":  "Welcome to your awesome auth endpoint",
         "success": True
     }
